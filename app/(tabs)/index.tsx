37fc683e8cbed0db9652dbb5a45e7d1c3a38cc42
handaruadit <adithandaru34@gmail.com>
2026-03-25 08:09:49 +0700

    Test2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Next Body</Text>
-      <Text>Heading2 Test</Text>
+      <Text>Heading2 Test2</Text>
     </View>
   );
 }
