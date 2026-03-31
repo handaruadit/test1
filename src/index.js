8f5868d4620b3e9d91323b0a4ee06b973e175259
rega1001 <90404466+rega1001@users.noreply.github.com>
2026-03-31 16:27:27 +0700

    v0.3 ke kompter bany

--- src/index.js
+++ src/index.js
@@ -17,17 +17,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
-// require("dotenv").config();
-// const db = require("./config/db");
-
-// async function testDB() {
-//   try {
-//     const res = await db.query("SELECT NOW()");
-//     console.log("Connected to DB:", res.rows[0]);
-//   } catch (err) {
-//     console.error("DB Error:", err.message);
-//   }
-// }
-
-// testDB();
