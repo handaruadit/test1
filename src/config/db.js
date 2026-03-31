70638dcd9f392acf41f57227c69e65caf563249d
rega1001 <90404466+rega1001@users.noreply.github.com>
2026-03-31 11:24:25 +0700

    v0.2 NeonDB

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,25 @@
 require("dotenv").config();
 const knex = require("knex");
 
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//   },
+// });
+
+// module.exports = db;
+
+
 const db = knex({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  connection: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
 
