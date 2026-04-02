d5ad072996015fa8147f74265c33e19f9259d2ea
littlebigfoot1453 <119672745+littlebigfoot1453@users.noreply.github.com>
2026-04-02 15:33:48 +0700

    v0.5 index for server

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const server = http.createServer(app);
 initSocket(server);
 
 const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
 
-server.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+server.listen(PORT, HOST, () => {
+  console.log(`🚀 Server API berjalan di http://${HOST}:${PORT}`);
 });
